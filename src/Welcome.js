fe2517b62988a939a29daefaa07e375ef856272c
James Dick <jimandjo.geo@yahoo.com>
2023-07-11 14:36:58 -0400

    Added to the Welcome Page

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,18 +1,26 @@
 import Card from 'react-bootstrap/Card';
 import Banner from "./Banner.jpg";
+import { Button } from 'react-bootstrap';
 
 function Welcome() {
   return (
-    <Card className="bg-dark text-white">
-      <Card.Img style={{ height:400 }}  src={Banner} alt="Card image" />
-      <Card.ImgOverlay>
-        <Card.Title></Card.Title>
+    <Card className="bg-info text-black align-center">
+      <Card.Img style={{ height:400 }}  src={Banner} alt="Encounter Church" />
+      <Card.Body>
+        <Card.Title>Pursuing the heart of God through divine encounters</Card.Title>
         <Card.Text>
-          {/* This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer. */}
+        Come join the Encounter family this Sunday at 9:30 as we worship our King and study his word together! <br></br>
         </Card.Text>
-        {/* <Card.Text>Last updated 3 mins ago</Card.Text> */}
-      </Card.ImgOverlay>
+        <Card.Text>107 E Marion St, Nappanee, IN 46550</Card.Text>
+        {/* <Button variant="info">Go somewhere</Button> */}
+        <Card.Text><p>July Schedule:<br></br></p>
+<p>July 2nd- Service 9:30<br></br>
+July 9th- Service 9:30<br></br>
+July 16th- Chair Cleaning Party!<br></br>
+July 23rd- Service 9:30<br></br>
+July 30th- Small Group<br></br></p>
+<p>*Chair Party: This will be held at the church in the parking lot. We need volunteers to help clean our new-to-us upholstered chairs! (Whoo-hoo!) Party will get started @10:00am and will finish off with some pizza for lunch! This will be a great opportunity for fellowship and to serve!</p></Card.Text>
+      </Card.Body>
     </Card>
   );
 }
