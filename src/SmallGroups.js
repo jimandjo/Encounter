4a16da7da2aed895139430d063075f66f523be89
James Dick <jimandjo.geo@yahoo.com>
2023-07-17 13:39:47 -0400

    Small Groups Page added.

--- src/SmallGroups.js
+++ src/SmallGroups.js
@@ -1,13 +1,21 @@
-function SmallGroups() {
-    return (
+import Card from 'react-bootstrap/Card';
+import Banner from "./Banner.jpg";
+import SmallGroupsPic from "./SmallGroupsPic.jpg"
 
-        <>
-        <h1>
-
-            Small Groups
-        </h1>
-        </>
-    )
+function Welcome() {
+  return (
+    <Card className="bg-info text-black align-center">
+      <Card.Img style={{ height:400 }}  src={SmallGroupsPic} alt="Encounter Church" />
+      <Card.Body>
+        <Card.Title>Small Groups</Card.Title>
+        <Card.Text>
+        Encounter Church of Nappanee meets twice a month in small groups.<br></br> 
+        This gives us the opportunity to grow in deeper relationships with each other.<br></br>  
+        Saturday night and Sunday morning groups available.<br></br>  
+        Let us know if you're interested and we can get you connected! Have a great weekend!</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
 
-export default SmallGroups
+export default Welcome;
