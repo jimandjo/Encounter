5c549c5a9400d9e2a7f058f06e9804c5135b2801
James Dick <jimandjo.geo@yahoo.com>
2023-07-17 13:26:31 -0400

    Text changes on About page.

--- src/About.js
+++ src/About.js
@@ -5,9 +5,9 @@ function About() {
   return (
     <Card className="bg-info text-black align-center">
       <Card.Img style={{ height:400 }}  src={WelcomePic} alt="Encounter Church" />
-      <Card.Body>About Encounter Church of Nappanee
-        <Card.Text><p>Welcome to Encounter Church of Nappanee!<br></br> 
-        The latest happenings and schedule of the church are listed on the first page of this site.<br></br> 
+      <Card.Body>Welcome to Encounter Church of Nappanee!
+        <Card.Text><p><br></br> 
+        The latest happenings and schedule of the church are listed on the first page of this site.<br></br> <br></br> 
         We currently meet the 1st and 3rd Sundays of every month at 107 E Marion St, Nappanee, IN 46550.<br></br> 
         The weeks we are not there we meet in small groups in members homes.<br></br> 
         For further information or details please message us through Facebook or email at encounter.nappanee@gmail.com.<br></br>
