2e1396a3a264901de318d4be7d6ee6133096cbf2
James Dick <jimandjo.geo@yahoo.com>
2023-07-17 12:24:08 -0400

    Commiting changes before pulling the merged files from github.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import About from './About';// import Welcome from './Welcome';
 import About from './About';
 import SmallGroups from './SmallGroups';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Team from './Team';
+import Welcome from './Welcome';
 import Welcome from './Welcome';
 
 
@@ -13,6 +15,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}>
+          <Route index element={<Welcome />}/>
           <Route index element={<Welcome />}/>
           <Route path="about" element={<About />} />
           <Route path='smallgroups' element={<SmallGroups />} />
