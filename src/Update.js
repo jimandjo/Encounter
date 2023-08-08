ee1e30ff71cc868f7a964c36dcfa6467b4e8dfb8
James Dick <jimandjo.geo@yahoo.com>
2023-08-08 14:00:29 -0400

    Added a form for each page that needs to be updated.

--- src/Update.js
+++ src/Update.js
@@ -1,21 +1,74 @@
-import { Button } from "react-bootstrap";
+import { Component } from "react";
+import { Button, Container } from "react-bootstrap";
 import  Form  from "react-bootstrap/Form";
 
-function Update() {
-    return (
+class SimpleForm extends Component {
+    render() {
+      return (
+      
+      <>
+        <Container>
+        <Form>
+            <legend>Edit Welcome Page</legend>
+          <Form.Group controlId="form.Line1">
+              <Form.Label>Edit Line 1</Form.Label>
+              <Form.Control type="text" placeholder="" />
+          </Form.Group>
+          <Form.Group controlId="form.Line2">
+              <Form.Label>Edit Line 2</Form.Label>
+              <Form.Control type="text" placeholder="" />
+          </Form.Group>
+          <Form.Group controlId="form.Calendar">
+              <Form.Label>Edit Calendar</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+          <Button>Update</Button>
+        </Form>
+      </Container>
 
-        <>
-            
-            <Form>
-            <Form.Group className="mb-3" controlId="WelcomeEdit">
-                <Form.Label>Edit Welcome</Form.Label>
-                <Form.Control type="text"></Form.Control>
-                <Button>Update Welcome</Button>
-            </Form.Group>
-            </Form>
-            
-        </>
-    )
-}
+<Container>
+<Form>
+    <legend>Edit About Page</legend>
+  <Form.Group controlId="form.Line1">
+      <Form.Label>Edit Line 1</Form.Label>
+      <Form.Control type="text" placeholder="" />
+  </Form.Group>
+  <Form.Group controlId="form.Line2">
+      <Form.Label>Edit Line 2</Form.Label>
+      <Form.Control type="text" placeholder="" />
+  </Form.Group>
+  <Form.Group controlId="form.About">
+      <Form.Label>Edit Body</Form.Label>
+      <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+  <Button>Update</Button>
+</Form>
+</Container>
 
-export default Update
+<Container>
+<Form>
+    <legend>Edit Small Groups Page</legend>
+  <Form.Group controlId="form.Line1">
+      <Form.Label>Edit Line 1</Form.Label>
+      <Form.Control type="text" placeholder="" />
+  </Form.Group>
+  <Form.Group controlId="form.Line2">
+      <Form.Label>Edit Line 2</Form.Label>
+      <Form.Control type="text" placeholder="" />
+  </Form.Group>
+  <Form.Group controlId="form.SmallGroups">
+      <Form.Label>Edit Body</Form.Label>
+      <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+  <Button>Update</Button>
+</Form>
+</Container>
+
+
+</>
+      );
+    }
+         
+  }
+  
+  export default SimpleForm;
