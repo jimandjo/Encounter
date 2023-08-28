9ccfcec60ddc1d226effbb35b033e9aea0471895
James Dick <jimandjo.geo@yahoo.com>
2023-08-28 08:26:19 -0400

    Beginning of Team Page

--- src/Team.js
+++ src/Team.js
@@ -1,3 +1,5 @@
+import { Button, Card } from "react-bootstrap"
+
 function Team() {
     return (
 
@@ -6,6 +8,17 @@ function Team() {
 
            Our Staff/Team
         </h1>
+
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Tom</Card.Title>
+        <Card.Text>
+         Our founding pastor
+        </Card.Text>
+        <Button variant="primary">Contact Tom</Button>
+      </Card.Body>
+    </Card>
         </>
     )
 }
