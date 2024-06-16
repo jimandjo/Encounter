3b441cd94d5b6da3081afe6ad6fdaafc6e129b64
James Dick <jimandjo.geo@yahoo.com>
2024-06-16 09:16:16 -0400

    Added a paragraph from Facebook to the main page.

--- src/Welcome.js
+++ src/Welcome.js
@@ -16,6 +16,7 @@ function Welcome() {
       <Card.Img style={{ height:400 }}  src={Banner} fluid alt="Encounter Church" />
       <Card.Body>
         <Card.Title>Pursuing the heart of God through divine encounters</Card.Title>
+        <Card.Text>When we were called to start a church we all received the same words, we were meant to look different and do “church” differently purely because we want what only the Father has for His/this body.  We threw out all we knew about the “western church” and are letting the Holy Spirit build what He wants. It’s been a hard but refreshing road to only go in directions the Holy Spirit is leading us in. We know we have to make moves and step out but we rely on Him to guide and direct us. The name Encounter came to us because Jesus is the only reason why we’re here. His heart is all we’re seeking.  We want this to be a place of encountering the King of Kings. We’re not running a business, we’re not controlling our finances like one, and we’re going all in for Him. Seeking his purity.</Card.Text>
         <Card.Text>
         Come join the Encounter family this Sunday at 9:30 as we worship our King and study his word together! <br></br>
         </Card.Text>
